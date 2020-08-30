ee3e40ff837866cab90f65dda29bfa6ef7bd70f5
Adam Hegedus <adamhegedus@vivaldi.net>
2020-08-30 22:09:32 +0200

    Add node skeleton

---
+++ src/test.spec.js
@@ -0,0 +1,12 @@
+describe('Test', () => {
+  it('should return true', () => {
+    // ASSIGN
+    const expected = true;
+
+    // ACT
+    const actual = true;
+
+    // ASSERT
+    expect(actual).toEqual(expected);
+  });
+});
